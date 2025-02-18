f6cb1623e6df5a82e58d77f8d392db0e9cdabb3e
longnguyendev <thanhlong210494@gmail.com>
2025-02-19 00:32:26 +0700

    chore: add theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router";
+import "./index.css";
 
 import "./i18n";
-import { router } from './router/router';
+import { router } from "./router/router";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./theme";
+import { CssBaseline } from "@mui/material";
 
-
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-     <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </StrictMode>
+);
